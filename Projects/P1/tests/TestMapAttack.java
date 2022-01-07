ecf4c9b4acce1f1e634a49bb9cf649864fd026ab
mikeko <geonhee.ko@gmail.com>
2022-01-07 16:10:10 -0500

    created a test for map.attack

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -4,6 +4,20 @@ import junit.framework.*;
 public class TestMapAttack {
 
 	public void testMapAttack() {
-		return null;	
+        MainFrame frame = new MainFrame();
+        Ghost ghost = frame.addGhost(new Location(2, 3), "mike", Color.red);
+        PacMan pacman = frame.addPacMan(new Location(2, 2));
+        //frame.startGame();
+        System.out.println("This should be true.");
+        assertTrue(ghost.myMap.attack());
+	}
+
+    public void testMapAttack1() {
+        MainFrame frame = new MainFrame();
+        Ghost ghost = frame.addGhost(new Location(1, 3), "mike", Color.red);
+        PacMan pacman = frame.addPacMan(new Location(2, 2));
+        //frame.startGame();
+        System.out.println("This should be false.");
+        assertFalse(ghost.myMap.attack());
 	}
 }
