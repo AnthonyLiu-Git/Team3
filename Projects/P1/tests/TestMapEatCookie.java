1fd491c4b25f450f45839a92f4b6e079ce4f19d7
Russell-Nesbitt <russell_nesbitt@yahoo.com>
2022-01-09 16:54:00 -0500

    eatCookie JUNIT test completed

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -4,7 +4,33 @@ import java.io.*;
 
 public class TestMapEatCookie {
 	
-	public void testMapEatCookie() {
-		return null;
+	public void testMapEatCookie()
+	{
+		MainFrame frame = new MainFrame();
+
+		Location myLocation = new Location(1, 1);
+	
+
+		PacMan pacman = frame.addPacMan(myLocation);
+
+
+		frame.startGame();
+
+
+		//Check if cookie was incremented
+		if(frame.getMap().getCookies() == 1)
+		{
+
+			//Note: This is all I can figure out how to test, as
+			//components, field, and locations are also changed
+			//but are private and have no getter methods.
+			
+			assertTrue(true);
+
+		}
+
+		assertTrue(false);
+
+
 	}
 }
