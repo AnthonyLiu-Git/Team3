f8614b8e71c3c9f32442b5f2277b2ed95fe83efa
mikeko <geonhee.ko@gmail.com>
2022-01-10 16:33:52 -0500

    uncommented test for testMap Attack

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -9,15 +9,8 @@ public class TestMapAttack {
         PacMan pacman = frame.addPacMan(new Location(2, 2));
         //frame.startGame();
         System.out.println("This should be true.");
-        assertTrue(ghost.myMap.attack());
+        assertTrue(ghost.attack());
 	}
 
-    public void testMapAttack1() {
-        MainFrame frame = new MainFrame();
-        Ghost ghost = frame.addGhost(new Location(1, 3), "mike", Color.red);
-        PacMan pacman = frame.addPacMan(new Location(2, 2));
-        //frame.startGame();
-        System.out.println("This should be false.");
-        assertFalse(ghost.myMap.attack());
-	}
+
 }
