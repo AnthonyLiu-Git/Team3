b59af49569bdb85c85e4a47696271e93a68ad66c
Mike Ko <geonhee.ko@gmail.com>
2022-01-10 16:37:04 -0500

    Update TestMapAttack.java

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -5,21 +5,14 @@ import java.io.*;
 public class TestMapAttack {
 
 	public void testMapAttack() {
-        // MainFrame frame = new MainFrame();
-        // Ghost ghost = frame.addGhost(new Location(2, 3), "mike", Color.red);
-        // PacMan pacman = frame.addPacMan(new Location(2, 2));
-        // frame.startGame();
-        // System.out.println("This should be true.");
-        // assertTrue(ghost.attack());
+        MainFrame frame = new MainFrame();
+        Ghost ghost = frame.addGhost(new Location(2, 3), "mike", Color.red);
+        PacMan pacman = frame.addPacMan(new Location(2, 2));
+        frame.startGame();
+        System.out.println("This should be true.");
+        assertTrue(ghost.attack());
 	}
 
-    public void testMapAttack1() {
-    //     MainFrame frame = new MainFrame();
-    //     Ghost ghost = frame.addGhost(new Location(1, 3), "mike", Color.red);
-    //     PacMan pacman = frame.addPacMan(new Location(2, 2));
-    //     //frame.startGame();
-    //     System.out.println("This should be false.");
-    //     assertFalse(ghost.attack());
-     }
+
 
 }
