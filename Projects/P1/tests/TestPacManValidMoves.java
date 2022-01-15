cb7ed49d77d6cf9aafa60a528ca22cd189906470
AnthonyLiu-Git <anthonyliu2013@gmail.com>
2022-01-15 13:04:22 -0500

    Update TestPacManValidMoves.java

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -12,12 +12,13 @@ public class TestPacManValidMoves extends TestCase {
 		ArrayList<Location> locations = new ArrayList<Location>();
 		locations.add(new Location(5,11));
 		locations.add(new Location(5,9));
-
+		ArrayList<Location> pacman_loc = pacman.get_valid_moves();
 		boolean containsAll = true;
 
-		for(Location l : pacman.get_valid_moves()) {
+		for(Location l : pacman_loc) {
 			if(!locations.contains(l)) {
 				containsAll = false;
+				break;
 			}
 		}
 
