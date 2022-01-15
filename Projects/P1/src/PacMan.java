6f8f94c85daaede7689b51d29536b753bdd3b97d
AnthonyLiu-Git <anthonyliu2013@gmail.com>
2022-01-15 13:23:00 -0500

    fix

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -22,7 +22,7 @@ public class PacMan{
 				if(Math.abs(i) != Math.abs(j)) {
 
                     HashSet <Map.Type> x = myMap.getLoc(myLoc.shift(i, j));
-					if(!x.contains(Type.WALL)) {
+					if(!x.contains(Map.Type.WALL)) {
 						
 						validMoves.add(new Location(myLoc.x+i, myLoc.y+j));
 					}
